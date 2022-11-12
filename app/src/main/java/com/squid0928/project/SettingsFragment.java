7ca4bfe3112bd8c516b7d33bc45f19d32bf796bb
yw824 <yongwoo25@gmail.com>
2022-11-12 19:35:41 +0900

    feat: [이용우] 설정 화면 구현

--- app/src/main/java/com/squid0928/project/SettingsFragment.java
+++ app/src/main/java/com/squid0928/project/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.squid0928.project;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,14 +22,11 @@ import android.view.ViewGroup;
  */
 public class SettingsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FirebaseUser user;
+    TextView userEmail;
+    TextView userName;
+    String idToken;
+    private DatabaseReference mDatabase;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -40,8 +44,6 @@ public class SettingsFragment extends Fragment {
     public static SettingsFragment newInstance(String param1, String param2) {
         SettingsFragment fragment = new SettingsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +51,29 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
+    // fragment의 레이아웃 로드
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        System.out.println(user.toString());
+        View view;
+        if(user != null) {
+            view = inflater.inflate(R.layout.fragment_settings, null);
+            userEmail = (TextView)view.findViewById(R.id.userEmail);
+            userEmail.setText(user.getEmail());
+
+            // mDatabase.child("firebaselogintest").child("UserAccount").
+            userName = (TextView)view.findViewById(R.id.userName);
+
+            return view;
+        }
+
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 }
