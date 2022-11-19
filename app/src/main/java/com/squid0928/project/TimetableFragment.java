9e80aba8cdab8e084a10bbfdef644bf495ea54b1
yw824 <yongwoo25@gmail.com>
2022-11-19 23:58:08 +0900

    feat: 시간표 입력 dialog 구현

--- app/src/main/java/com/squid0928/project/TimetableFragment.java
+++ app/src/main/java/com/squid0928/project/TimetableFragment.java
@@ -1,5 +1,8 @@
 package com.squid0928.project;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,6 +19,10 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.sql.Array;
+import java.sql.Time;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,7 +33,7 @@ public class TimetableFragment extends Fragment {
 
     FirebaseUser user;
     private DatabaseReference mDatabase;
-    View[][] classes;
+    ArrayList<ArrayList<TextView>> classes;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -73,37 +81,92 @@ public class TimetableFragment extends Fragment {
         // Inflate the layout for this fragment
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
+        // 버튼들을 객체로 받기 위한 배열
+        classes = new ArrayList<>();
+
         user = FirebaseAuth.getInstance().getCurrentUser();
         View view;
         if(user != null) {
             view = inflater.inflate(R.layout.fragment_timetable, null);
 
-            int i = 0;
-            classes = new View[10][];
-            for(i = 0; i < 10; i++)
-                classes[i] = new View[5]; // 여기까지 OK
-
-
+            // 각 tablerow를 가져온 다음 for문을 통해 각 tablerow에 담긴 textview들을 추가한다.
             TableRow am09 = view.findViewById(R.id.am09);
+            addTableViews(am09);
             TableRow am10 = view.findViewById(R.id.am10);
+            addTableViews(am10);
             TableRow am11 = view.findViewById(R.id.am11);
+            addTableViews(am11);
             TableRow pm12 = view.findViewById(R.id.pm12);
+            addTableViews(pm12);
             TableRow pm13 = view.findViewById(R.id.pm13);
+            addTableViews(pm13);
             TableRow pm14 = view.findViewById(R.id.pm14);
+            addTableViews(pm14);
             TableRow pm15 = view.findViewById(R.id.pm15);
+            addTableViews(pm15);
             TableRow pm16 = view.findViewById(R.id.pm16);
+            addTableViews(pm16);
             TableRow pm17 = view.findViewById(R.id.pm17);
+            addTableViews(pm17);
             TableRow pm18 = view.findViewById(R.id.pm18);
+            addTableViews(pm18);
 
             return view;
         }
         return inflater.inflate(R.layout.fragment_timetable, container, false);
     }
 
+    void addTableViews(TableRow row) {
+        if(row == null) return;
+
+        TextView view;
+        int j;
+        ArrayList<TextView> input = new ArrayList<>();
+        for(j = 0; j < row.getChildCount(); ++j) {
+            view = (TextView)row.getVirtualChildAt(j); // 각 자식들을 받아내고
+            if(j != 0) // 시간 축이 아니면 리스너 추가
+                view.setOnClickListener(classListener); // 텍스트뷰 클릭 시 리스너 추가
+            input.add(view);
+            // System.out.println(((TextView)am09.getVirtualChildAt(j)).getText().toString());
+        }
+        classes.add(input);
+    }
+
     View.OnClickListener classListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Toast.makeText(getActivity(), "Button Clicked", Toast.LENGTH_LONG).show();
+            View dialogView = getLayoutInflater().inflate(R.layout.dialog_timetable, null);
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("시간표 설정").setMessage("시간표 설정");
+            builder.setView(dialogView);
+
+            EditText lectureName = (EditText)dialogView.findViewById(R.id.lectureName);
+            EditText lectureRoom = (EditText)dialogView.findViewById(R.id.lectureRoom);
+
+            TextView currentView = (TextView)v;
+
+            builder.setPositiveButton("추가하기", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int pos) {
+                    String name = lectureName.getText().toString();
+                    String room = lectureRoom.getText().toString();
+                    currentView.setText(name + "\n" + room);
+
+                    int red = (int)(Math.random() * 125) + 125;
+                    int blue = (int)(Math.random() * 125) + 125;
+                    int green = (int)(Math.random() * 125) + 125;
+                    currentView.setBackgroundColor(Color.rgb(red, green, blue));
+                }
+            });
+            builder.setNegativeButton("되돌아가기", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    return;
+                }
+            });
+
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
         }
     };
 }
